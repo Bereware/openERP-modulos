49f43489b5d214114fdbe3b31c4f5b0a82ae61ba
Bereware <bereware@gmail.com>
2016-11-01 23:35:47 +0100

    Add class to init to Gym

--- Gym/__init__.py
+++ Gym/__init__.py
@@ -20,7 +20,6 @@
 #    along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-<<<<<<< HEAD
+
 import gymclass
-=======
->>>>>>> 2cb7a369ff2cbcdc6daecc14141f18c05fb590fc
+
