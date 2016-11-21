824b98eaa285ce8e051808ef4f583452b6af2693
Bereware <bereware@gmail.com>
2016-11-21 17:34:13 +0100

    error

--- gym_expansion/__init__.py
+++ gym_expansion/__init__.py
@@ -19,7 +19,3 @@
 #    along with this program.  If not, see http://www.gnu.org/licenses/.
 #
 ##############################################################################
-
-import gymclass
-import gymuser
-import gyminstructor
